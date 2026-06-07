33b27eb2e3b21d386690400bcf265094ac5fdb72
haidang <lighthouselp95@gmail.com>
2026-06-07 22:06:12 +0700

    them referrer facebook

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.use((req, res, next) => {
             source: referer,
             ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
             url: req.originalUrl,
-            host: req.headers.host,
             'sec-fetch-mode': req.headers['sec-fetch-mode'],
             'sec-fetch-site': req.headers['sec-fetch-site'],
             country: req.headers['cf-ipcountry'],
